6587ad3e59060babe4b7f7f57269f9c3adb2c472
Proch92 <proch92@gmail.com>
2015-03-18 15:03:38 +0100

    first assignment 1

--- assignment1.hs
+++ assignment1.hs
@@ -1,9 +1,31 @@
 data Elem = Dot | Star
 
+---------------- Dichiarazione variabile "vec" ---------------
+vec :: [Elem]
+vec = [Dot, Star, Dot, Dot, Dot, Dot, Star, Star, Dot, Dot]
+
+---------------- countStar -----------------------
 countStar :: [Elem] -> Int
 countStar [] = 0
 countStar (Star:xs) = 1 + countStar xs
 countStar (Dot:xs) = countStar xs
 
-vec :: [Elem]
-vec = [Dot, Star, Dot, Star, Star]
+---------------- printableSeq --------------------
+printableSeq :: [Elem] -> [Char]
+printableSeq [] = ""
+printableSeq (Star:xs) = '*' : printableSeq xs
+printableSeq (Dot:xs) = '.' : printableSeq xs
+
+--------------- swapSeq -------------------
+swapSeq :: [Elem] -> [Elem]
+swapSeq [] = []
+swapSeq (Star:xs) = Dot : swapSeq xs
+swapSeq (Dot:xs) = Star : swapSeq xs
+
+------------- zipSeq ------------------
+zipSeq :: [Elem] -> [Elem]
+zipSeq [] = []
+zipSeq (Star:xs) = Star : zipSeq xs
+zipSeq (Dot:(Dot:xs)) = zipSeq (Dot:xs)
+zipSeq (Dot:(Star:xs)) = Dot : (Star : zipSeq xs)
+zipSeq (Dot:[]) = [Dot]
